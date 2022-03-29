67057fd25f4dba320a28ea0af2ffb6feb4c59dc4
smpny7 <symphonymode7@icloud.com>
2022-03-29 22:10:51 +0900

    feat: 🎸 楽曲再生機能

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,125 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { data } from 'data'
+import { getDownloadURL, ref } from 'firebase/storage'
+import { useState } from 'react'
+import ReactJkMusicPlayer, {
+    ReactJkMusicPlayerInstance,
+    ReactJkMusicPlayerProps,
+} from 'react-jinke-music-player'
+import { Button, Container, FlexboxGrid, List, Navbar } from 'rsuite'
+import { firebaseStorage } from './firebase/init'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const handleDownload = (src: string) => {
+        const uri = ref(firebaseStorage, src)
+        return getDownloadURL(uri)
+    }
+
+    const [audioInstance, setAudioInstance] =
+        useState<ReactJkMusicPlayerInstance>()
+
+    const getAudioInstance = (instance: ReactJkMusicPlayerInstance) => {
+        setAudioInstance(instance)
+    }
+
+    const audioLists = data.map((item) => {
+        return {
+            name: item.name,
+            singer: item.singer,
+            cover: item.cover,
+            musicSrc: () => handleDownload(item.song),
+            lyrics: item.lyrics,
+        }
+    })
+
+    const playerOptions: ReactJkMusicPlayerProps = {
+        audioLists: audioLists,
+        autoPlay: false,
+        defaultPlayMode: 'singleLoop',
+        getAudioInstance: getAudioInstance,
+        glassBg: true,
+        mode: 'full',
+        remember: true,
+        remove: false,
+        showThemeSwitch: false,
+        showLyric: true,
+        showMediaSession: true,
+        spaceBar: true,
+        toggleMode: false,
+        theme: 'light',
+    }
+
+    return (
+        <div>
+            <Navbar appearance="inverse">
+                <Navbar.Brand>KIKI-ATE</Navbar.Brand>
+            </Navbar>
+            <ReactJkMusicPlayer {...playerOptions} />
+            <button
+                className="fixed top-0 right-0 z-20"
+                onClick={() => {
+                    if (audioInstance && audioInstance.destroy)
+                        audioInstance.destroy()
+                }}
+            >
+                destroy player
+            </button>
+            <Container>
+                <List className="pb-20" hover>
+                    {data.map((item, index) => (
+                        <List.Item key={item.name} index={index + 1}>
+                            <FlexboxGrid>
+                                {/* Cover */}
+                                <FlexboxGrid.Item colspan={2}>
+                                    <img
+                                        src={item.cover}
+                                        alt={item.name}
+                                        className="mx-auto w-1/2 rounded"
+                                    />
+                                </FlexboxGrid.Item>
+
+                                {/* Name */}
+                                <FlexboxGrid.Item colspan={4}>
+                                    <h2 className="text-lg">{item.name}</h2>
+                                </FlexboxGrid.Item>
+
+                                {/* Lyrics */}
+                                <FlexboxGrid.Item colspan={12}>
+                                    <p>{item.lyrics}</p>
+                                </FlexboxGrid.Item>
+
+                                {/* Singer */}
+                                <FlexboxGrid.Item colspan={2}>
+                                    <p>{item.singer}</p>
+                                </FlexboxGrid.Item>
+
+                                {/* Year */}
+                                <FlexboxGrid.Item colspan={2}>
+                                    <p>{item.year}</p>
+                                </FlexboxGrid.Item>
+
+                                {/* Button */}
+                                <FlexboxGrid.Item colspan={2}>
+                                    <Button
+                                        onClick={() => {
+                                            if (
+                                                audioInstance &&
+                                                audioInstance.updatePlayIndex
+                                            )
+                                                audioInstance.updatePlayIndex(
+                                                    index,
+                                                )
+                                        }}
+                                    >
+                                        再生
+                                    </Button>
+                                </FlexboxGrid.Item>
+                            </FlexboxGrid>
+                        </List.Item>
+                    ))}
+                </List>
+            </Container>
+        </div>
+    )
 }
 
-export default App;
+export default App
